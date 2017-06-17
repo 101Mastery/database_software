e303cbea588376e537dd9b63aa1d75e81f461a2f
DAINGER_PC\dainger <sonic2fire@gmail.com>
2017-06-17 02:49:22 -0600

    fix

--- project/__init__.py
+++ project/__init__.py
@@ -6,7 +6,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-import home
-import formulas.formula_views
-import login.login_view
-import user.user_creator_menu
+import project.home
+import project.formulas.formula_views
+import project.login.login_view
+import project.user.user_creator_menu
